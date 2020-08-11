46d830992d54707a740b61ea2ebe1cb9fb8b90f1
yuanhan <1484347431@qq.com>
2020-08-11 16:05:37 +0800

    1

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,26 +1,36 @@
 <template>
-    <body>
-        <div>
-
-            <div class="left_bar_list">
-                <li>User</li>
-                    </div>
-                <div class="top_bar_news">
-                    <a class="new_artical" href="https://baidu.com" aria-label="友情链接" target="_blank">新公告</a>
-                    <a class="new_info" href="https://google.com" aria-label="谷歌链接" target="_blank">Announcing API</a>
-                        <div class="top_bar_action">
-                            <a class="top_action" href="/support/" target="_blank">帮助</a>
-                        </div>
-                </div>
-
-            <div style="height:auto">
-                <h1>Welcome</h1>
-                <map name="map">
-                    <li><a href="./../">Back</a></li>
-                    <li></li>
-                </map>
-            </div>
+  <div>
+    <div class="left_bar_list">
+      <li>User</li>
+    </div>
+    <div class="container">
+      <div class="top_bar_news">
+        <a
+          class="new_artical"
+          href="https://baidu.com"
+          aria-label="友情链接"
+          target="_blank"
+          >新公告</a
+        >
+        <a
+          class="new_info"
+          href="https://google.com"
+          aria-label="谷歌链接"
+          target="_blank"
+          >Announcing API</a
+        >
+        <div class="top_bar_action">
+          <a class="top_action" href="/support/" target="_blank">帮助</a>
         </div>
-    </body>
-</template>
+      </div>
 
+      <div style="height:auto">
+        <h1>Welcome</h1>
+        <map name="map">
+          <li><a href="./../">Back</a></li>
+          <li></li>
+        </map>
+      </div>
+    </div>
+  </div>
+</template>
