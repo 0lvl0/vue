e3ef8b2199843c11697544b881ea0882dca22315
yuanhan <1484347431@qq.com>
2020-08-14 15:19:55 +0800

    左侧边栏高度校准

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <!-- 左侧栏设置 -->
     <div class="left_bar_list">
-      <img class="left_logo" src="./../assets/logo.png" alt="Surreal">
+      <img class="left_logo" src="./../assets/logo.png" alt="Surreal" />
       <div style="height:30px"></div>
       <a href="/dashboard">Dashboard</a>
       <a href="/profiles">Profiles</a>
@@ -9,7 +10,9 @@
       <a href="/support">Support</a>
       <a href="/account">Account</a>
     </div>
+    <!-- 右侧整体容器布局 -->
     <div class="container">
+      <!-- 顶部边栏设置 -->
       <div class="top_bar_news">
         <a
           class="new_artical"
@@ -25,11 +28,12 @@
           target="_blank"
           >Announcing API</a
         >
+        <!-- 右上角按钮 -->
         <div class="top_bar_action">
           <a class="top_action" href="/support/" target="_blank">帮助</a>
         </div>
       </div>
-
+      <!-- 正文内容区域 -->
       <div style="height:auto">
         <h1>Welcome</h1>
         <map name="map">
@@ -42,14 +46,22 @@
 </template>
 <style>
 .left_bar_list a {
+  padding-bottom: 50px;
+  padding-top: 50px;
   text-decoration: none;
-  height: 100px;
   width: 135px;
   overflow: hidden;
-  text-align: center;
-  border: 90px;
-  align-content: center;
-  display: grid;
+  align-self: center;
+  display: flex;
   color: black;
+  flex-direction: column;
+}
+a .new_artical {
+  text-align: center;
+  display: flex;
+}
+a .new_info {
+  text-align: center;
+  display: flex;
 }
 </style>
